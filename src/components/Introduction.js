0ada64f88402b44f9051ec9e3b2e43e2a21150bc
xinyueli <hecatelixyue@gmail.com>
2025-08-14 23:26:02 -0400

    update introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,7 +5,7 @@ import { intro_container, intro_text, intro_image} from './Introduction.module.c
 const Introduction = () => {
     return (
         <div className={intro_container}>
-            <p className={intro_text}>I’m a fourth-year Computer Science student at the University of Toronto with a strong passion for software development. I enjoy building impactful, user-focused applications and exploring innovative technologies. Always eager to learn and collaborate, I’m excited to contribute my skills to meaningful projects in the tech world.</p>
+            <p className={intro_text}>I’m a fourth-year Computer Science student at the University of Toronto with a strong passion for software development. I enjoy building impactful, user-focused applications and exploring innovative technologies. I’m excited to contribute my skills to meaningful projects in the tech world.</p>
             <StaticImage className={intro_image}
             src="../images/intro.jpg"/>
         </div>
